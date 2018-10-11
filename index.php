e9e28014d2058e00f414e216b90bca54045dc7c1
Acta86Neon <marco.meichtry.1986@gmail.com>
2018-10-11 14:11:37 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AJAX Tutorial PHP</title>
+
+    <script type="text/javascript"
+            src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,900'
+          rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+</head>
+<body>
+
+<div class="ud_box">
+    <form id="ud_form">
+        <input type="text" name="firstName" placeholder="Firstname">
+        <input type="text" name="lastName" placeholder="Lastname">
+        <input type="submit" name="send" id="send">
+    </form>
+</div>
+
+<div class="ud_box" id="ud_output"></div>
+
+</body>
+</html>
